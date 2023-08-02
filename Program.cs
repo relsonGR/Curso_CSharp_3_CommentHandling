f0aaa770f35b8094465a9dbe1a5bb62721cb9a49
relsonGR <riverarelson99@gmail.com>
2023-08-02 11:41:10 -0600

    El ejercicio #3 fue terminado

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Curso_CSharp_3_CommentHandling
+{
+    internal static class Program
+    {
+        /// <summary>
+        /// Punto de entrada principal para la aplicación.
+        /// </summary>
+        /// 
+
+        //Esto es un comentario de una sola linea
+
+        /* Aqui comienza el comentario
+         * 
+         * 
+         * 
+         * 
+         * 
+         * Aqui termina comentario
+         */
+
+
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();   //ejemplo esta linea es importante XD
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1()); //Ejecuta el form principal
+        }
+    }
+}
